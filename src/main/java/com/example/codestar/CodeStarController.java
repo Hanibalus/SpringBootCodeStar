8468b3c353ec46260a056a372697d17ffa968c9e
POPA Eugeniu Leonard <eugen.popa@orange.com>
2020-08-13 17:55:05 +0300

    Initial

---
+++ src/main/java/com/example/codestar/CodeStarController.java
@@ -0,0 +1,14 @@
+package com.example.codestar;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CodeStarController {
+
+    @RequestMapping(value = "/say", method = RequestMethod.GET)
+    public String sayHello(){
+        return "Hello AwsCodeStar!";
+    }
+}
---
+++ src/main/java/com/example/codestar/CodestarApplication.java
@@ -0,0 +1,13 @@
+package com.example.codestar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodestarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodestarApplication.class, args);
+    }
+
+}
